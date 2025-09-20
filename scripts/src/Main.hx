3eeb3c7650b933d8ad29995b6df4e986c32dfd13
HackDev <80620538+Hackx2@users.noreply.github.com>
2025-09-20 13:47:47 +0100

    testing fix

--- scripts/src/Main.hx
+++ scripts/src/Main.hx
@@ -1,100 +1,104 @@
 package;
 
-#if !js
+import mini.Ini;
+import mini.Parser;
+
+#if js
+import haxe.http.HttpJs;
+#else
 import sys.io.File;
 #end
-import mini.*;
 
 class Main {
-	static inline function separate(val:String):Void {
-		return trace('-------- $val');
-	}
+    static inline function heading(val:String):Void {
+        trace('-------- $val');
+    }
 
-	static function main():Void {
-		trace('<---------------------- Testing ---------------------->\n');
-		final ini:Ini = #if js Parser.parse('[main.test]
-			name="hackx2"
-			cutie="milo"
+    static function main():Void {
+        trace('<---------------------- Testing ---------------------->\n');
+        testNormalIniParsing();
+        testLongIniParsing();
+        testIniCreation();
 
-			[General]
-			version=1.0
-			theme=dark
+		trace(mini.EntryType.fromString('Document'));
+    }
 
-			[User:General]
-			username="Milo"
-			role=Admin
+    static function testNormalIniParsing():Void {
+        final ini:Ini = Parser.parse(
+            #if js
+            HttpJs.requestUrl('resources/testing.ini')
+            #else
+            File.getContent('resources/testing.ini')
+            #end
+        );
 
-			[User2:General]
-			username="Orbl"
-			role=User
-			theme="Gold"') #else Parser.parse(File.getContent('resources/testing.ini')) #end;
-		//
-		// Normal .ini parsing test!
-		//
+        final main:Ini = ini.elementsNamed("main.test").next();
+        heading('Global');
+        trace(main.get('name'));
+        trace(main.get('cutie'));
 
-		// NORMAL ELEMENT TESTING
-		final main:Ini = ini.elementsNamed("main.test").next();
-		separate('Global');
-		trace(main.get('name'));
-		trace(main.get('cutie'));
+        final general:Ini = ini.elementsNamed("General").next();
+        heading('[General] normal');
+        trace(general.get("version"));
+        trace(general.get("theme"));
 
-		// USER ELEMENT & INHERITED TESTING
-		final general:Ini = ini.elementsNamed("General").next();
-		separate('[General] normal');
-		trace(general.get("version"));
-		trace(general.get("theme"));
+        trace("\nAll tests passed!\n");
+    }
 
-		// END TEST!
-		trace("\nAll tests passed!\n");
+    static function testLongIniParsing():Void {
+        #if js
+        final cc = HttpJs.requestUrl("resources/testing_long.ini");
+        if (cc != null) {
+            parseAndTraceLongIni(Parser.parse(cc));
+        }
+        #else
+        if (sys.FileSystem.exists('resources/testing_long.ini')) {
+            parseAndTraceLongIni(Parser.parse(File.getContent('resources/testing_long.ini')));
+        }
+        #end
+    }
 
-		#if !js
-		//
-		// Long .ini file parsing test
-		//
-		if (sys.FileSystem.exists('resources/testing_long.ini')) {
-			final ini:Ini = Parser.parse(File.getContent('resources/testing_long.ini'));
-			for (section in ini.elements()) {
-				if (section.nodeType == Comment)
-					continue;
-				separate('[' + section.nodeName + ']');
-				for (key in section.keys())
-					trace('${key.nodeName} = ${section.get(key.nodeName)}');
-				trace('');
+    static function parseAndTraceLongIni(ini:Ini):Void {
+        for (section in ini.elements()) {
+            if (section.nodeType == Comment)
+                continue;
+            heading('[' + section.nodeName + ']');
+            for (key in section.keys())
+                trace('${key.nodeName} = ${section.get(key.nodeName)}');
+            trace('');
 
-				for (child in section.find(node -> node.nodeName == "welcome message")) {
-					trace("FOUND IT: " + child.nodeValue);
-				}
-			}
-			trace("\nduh long file test!\n");
-			separate('[Sending Full long.ini tostring]');
-			trace(ini.toString());
-		}
-		#end
+            for (child in section.find(node -> node.nodeName == "welcome message")) {
+                trace("FOUND IT: " + child.nodeValue);
+            }
+        }
+        trace("\nduh long file test!\n");
+        heading('[Sending Full long.ini tostring]');
+        trace(ini.toString());
+    }
 
-		//
-		// Creating a .ini file using haxe
-		//
-		final ini:Ini = Ini.createDocument();
+    static function testIniCreation():Void {
+        final ini:Ini = Ini.createDocument();
 
-		final general:Ini = Ini.createSection("General");
-		general.set("name", "MyApp");
-		general.set("version", "1.0");
-		general.comment("This is gay");
-		ini.addChild(general);
+        final general:Ini = Ini.createSection("General");
+        general.set("name", "MyApp");
+        general.set("version", "1.0");
+        general.comment("This is gay");
+        ini.addChild(general);
 
-		final user:Ini = Ini.createSection("User");
-		user.set("username", "Milo");
-		user.set("role", "Admin");
-		ini.addChild(user);
+        final user:Ini = Ini.createSection("User");
+        user.set("username", "Milo");
+        user.set("role", "Admin");
+        ini.addChild(user);
 
-		final net:Ini = Ini.createSection("Network");
-		net.set("ip", "192.168.0.5");
-		net.set("port", "8080");
-		ini.addChild(net);
+        final net:Ini = Ini.createSection("Network");
+        net.set("ip", "192.168.0.5");
+        net.set("port", "8080");
+        ini.addChild(net);
 
-		final miloNetworkInherit:Ini = Ini.createSection("Milo:Network", ini);
-		miloNetworkInherit.set("hi", "heyo!!!");
-		ini.addChild(miloNetworkInherit);
-		trace(ini.toString());
-	}
+        final miloNetworkInherit:Ini = Ini.createSection("Milo:Network", ini);
+        miloNetworkInherit.set("hi", "heyo!!!");
+        ini.addChild(miloNetworkInherit);
+
+        trace(ini.toString());
+    }
 }
