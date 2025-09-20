5f14aaf13905e64ac9cd93571e49fe6202600611
HackDev <80620538+Hackx2@users.noreply.github.com>
2025-09-20 14:03:35 +0100

    add `extraParams.hxml` & `LICENSE`

--- submit.bat
+++ submit.bat
@@ -4,8 +4,10 @@ set PATH=C:\Program Files\7zip;%PATH%
 rmdir /s /q release.zip
 rmdir /s /q release
 mkdir release
+copy LICENSE release\
 copy haxelib.json release\
 copy README.md release\
+copy extraParams.hxml release\
 mkdir release\mini
 xcopy mini\*.hx release\mini\ /Y
 7za a -tzip release.zip release
