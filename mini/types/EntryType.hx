05a4698b7f296542219c4c62ca4d90f74db3293a
HackDev <80620538+Hackx2@users.noreply.github.com>
2025-09-28 16:36:42 +0100

    moving `EntryType`

---
+++ mini/types/EntryType.hx
@@ -0,0 +1,51 @@
+package mini.types;
+
+enum abstract EntryType(Int) {
+	final Document:EntryType = 0x00; // 0
+
+	/**
+	 * Section
+	 * Example: [BlahBlahBlah]
+	 */
+	final Section:EntryType = 0x01; // 1
+
+	/**
+	 * Comment
+	 * Example: ;BlahBlahBlah
+	 */
+	final Comment:EntryType = 0x02; // 2
+
+	/**
+	 * Key Value
+	 * Example: BlahBlahBlah = WhatTheSigma
+	 */
+	final KeyValue:EntryType = 0x03; // 3
+
+	/**
+	 * [Description] Convert type to string.
+	 * @return String
+	 */
+	@:to public function toString():String {
+		return switch (cast this : EntryType) {
+			case Document: "Document";
+			case Section: "Section";
+			case Comment: "Comment";
+			case KeyValue: "KeyValue";
+		};
+	}
+
+	/**
+	 * [Description] Converts a string to a type.
+	 * @param str Option
+	 * @return EntryType
+	 */
+	@:from public static function fromString(str:String):EntryType {
+		return switch str.toLowerCase() {
+			case "document": Document;
+			case "section": Section;
+			case "comment": Comment;
+			case "keyvalue": KeyValue;
+			default: throw 'Unknown Type: $str';
+		};
+	}
+}
